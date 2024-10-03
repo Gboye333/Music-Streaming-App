550d5d85c14b591770ee7fe5251b0fdbdaa44721
Joseph <gboyes333@gmail.com>
2024-10-03 12:49:53 +0100

    updated code

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import { albumsData } from '../assets/assets'
 import AlbumItems from './AlbumItems'
 import { songsData } from '../assets/assets'
+import SongItem  from './SongItem'
 
 const DisplayHome = () => {
   return (
@@ -15,11 +16,10 @@ const DisplayHome = () => {
       key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
       </div>
     </div>
-    <Navbar/>
     <div className='mb-4'>
       <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
       <div className='flex overflow-auto'>
-      {songsData.map((item,index))}
+      {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
       </div>
     </div>
     </>
--- src/components/songItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const songItem = ({name,image,desc,id}) => {
-  return (
-    <div className='min-w-[180px]'></div>
-  )
-}
-
-export default songItem
